e91bfb94db43e5012cf02cdc1f6b9d540f714be8
geneyee <geneyeey@gmail.com>
2023-06-17 11:07:52 +0900

    시험성적 9498번

---
+++ src/step2/_2_Q_9498.java
@@ -0,0 +1,22 @@
+package step2;
+
+import java.util.Scanner;
+
+public class _2_Q_9498 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if (num >= 90) {
+            System.out.println("A");
+        } else if (num >= 80 && num < 90) {
+            System.out.println("B");
+        } else if (num >= 70 && num < 80) {
+            System.out.println("C");
+        } else if (num >= 60 && num < 70) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
