43dbec300c6340567198f486fd9baba4e6898d1e
geneyee <geneyeey@gmail.com>
2023-06-17 17:34:37 +0900

    구구단 2739번

---
+++ src/step3/_1_Q_2739.java
@@ -0,0 +1,20 @@
+package step3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class _1_Q_2739 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(N + " * " + i + " = " + N * i);
+        }
+
+        br.close();
+    }
+}
