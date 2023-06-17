a023e3ec9eb8ac5d6d5dffd2d3d8a3c1130a46bc
geneyee <geneyeey@gmail.com>
2023-06-17 18:09:41 +0900

    반복문 25314번

---
+++ src/step3/_5_Q_25314.java
@@ -0,0 +1,15 @@
+package step3;
+
+import java.util.Scanner;
+
+public class _5_Q_25314 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N/4; i++) {
+            System.out.print("long ");
+        }
+        System.out.println("int");
+    }
+}
