837fb15d74f03566759f1147ecae062f1456b418
geneyee <geneyeey@gmail.com>
2023-06-17 17:57:30 +0900

    반복문 영수증 25304번

---
+++ src/step3/_4_Q_25304.java
@@ -0,0 +1,21 @@
+package step3;
+
+import java.util.Scanner;
+
+public class _4_Q_25304 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int N = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            sum += (a * b);
+        }
+        System.out.println(X == sum ? "Yes" : "No");
+    }
+}
