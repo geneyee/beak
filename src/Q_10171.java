6bb63bbc9f0fae8bbfd9120cdddcfe2660a48d44
geneyee <geneyeey@gmail.com>
2023-06-17 10:27:58 +0900

    Q)10171

---
+++ src/Q_10171.java
@@ -0,0 +1,14 @@
+public class Q_10171 {
+    /*
+    \    /\
+     )  ( ')
+    (  /  )
+     \(__)|
+     */
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
