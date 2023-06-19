1260e50d02045031ed68bd7553a15894147a7540
geneyee <geneyeey@gmail.com>
2023-06-19 09:54:45 +0900

    10807번

---
+++ src/step4/_1_Q_10807.java
@@ -0,0 +1,25 @@
+package step4;
+
+import java.util.Scanner;
+
+public class _1_Q_10807 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+           arr[i] = sc.nextInt();
+        }
+
+        int v = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            if(arr[i] == v){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
