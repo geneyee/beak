d7d0c4e386fbd808b668a622210cd782bae0f66a
geneyee <geneyeey@gmail.com>
2023-06-22 09:53:00 +0900

    HashSet 3052번

--- src/step4/_8_Q_3052.java
+++ src/step4/_8_Q_3052.java
@@ -1,23 +1,18 @@
 package step4;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class _8_Q_3052 {
     public static void main(String[] args) {
+        // HashSet은 중복값 저장 xx
         Scanner sc = new Scanner(System.in);
-        int[] num = new int[10];
+        HashSet<Integer> h = new HashSet<Integer>();
 
-        for (int i = 0; i < num.length; i++) {
-            num[i] = sc.nextInt();
-            num[i] %= 42;
+        for (int i = 0; i < 10; i++) {
+            h.add(sc.nextInt()%42);
         }
-
-        int count = 0;
-
-        for (int i = 0; i < num.length; i++) { //10
-
-        }
-        System.out.println(count);
-
+        sc.close();
+        System.out.println(h.size());
     }
 }
